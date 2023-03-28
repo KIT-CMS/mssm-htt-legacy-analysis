1f52fbc630a94e2a64b6092dc928c9cfb630aebd
Maximilian Burkart <maxiburkart@gmail.com>
2023-03-28 16:01:10 +0200

    Update workflow for control plots with systematics - additional

--- shapes/extract_postfit_shapes.sh
+++ shapes/extract_postfit_shapes.sh
@@ -4,6 +4,9 @@ ERA=$1
 CHANNEL=$2
 VARIABLE=$3
 
+USE_MC=$4
+[[ -z $4 ]] && USE_MC=0
+
 source utils/setup_cmssw.sh
 
 if [[ ! -d output/shapes/${ERA}-${CHANNEL}-control-datacard-shapes-prefit/ ]]
@@ -11,4 +14,14 @@ then
     mkdir -p output/shapes/${ERA}-${CHANNEL}-control-datacard-shapes-prefit/
 fi
 
-PostFitShapesFromWorkspace -m 125 -w output/workspaces/${ERA}-${CHANNEL}-${VARIABLE}-control-workspace.root -d output/datacards/${ERA}-${CHANNEL}-${VARIABLE}-control/_${VARIABLE}/${ERA}/htt_${CHANNEL}_300_${ERA}/combined.txt.cmb -o output/shapes/${ERA}-${CHANNEL}-control-datacard-shapes-prefit/${ERA}-${CHANNEL}-${VARIABLE}-control-datacard-shapes-prefit.root
+if [[ "${USEMC}" == "1" ]]; then
+    PostFitShapesFromWorkspace -m 125 \
+        -w output/workspaces/${ERA}-${CHANNEL}-${VARIABLE}-mc-control-workspace.root \
+        -d output/datacards/${ERA}-${CHANNEL}-${VARIABLE}-mc-control/_${VARIABLE}/${ERA}/htt_${CHANNEL}_300_${ERA}/combined.txt.cmb \
+        -o output/shapes/${ERA}-${CHANNEL}-control-datacard-shapes-prefit/${ERA}-${CHANNEL}-${VARIABLE}-mc-control-datacard-shapes-prefit.root
+else
+    PostFitShapesFromWorkspace -m 125 \
+        -w output/workspaces/${ERA}-${CHANNEL}-${VARIABLE}-control-workspace.root \
+        -d output/datacards/${ERA}-${CHANNEL}-${VARIABLE}-control/_${VARIABLE}/${ERA}/htt_${CHANNEL}_300_${ERA}/combined.txt.cmb \
+        -o output/shapes/${ERA}-${CHANNEL}-control-datacard-shapes-prefit/${ERA}-${CHANNEL}-${VARIABLE}-control-datacard-shapes-prefit.root
+fi
